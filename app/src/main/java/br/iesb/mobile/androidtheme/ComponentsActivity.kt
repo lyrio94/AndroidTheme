5394f78779e875423fbef202f9862b23adb9613f
lyrio94 <alexandrelyrio94@hotmail.com>
2021-03-02 16:13:13 -0300

    first commit

---
+++ app/src/main/java/br/iesb/mobile/androidtheme/ComponentsActivity.kt
@@ -0,0 +1,11 @@
+package br.iesb.mobile.androidtheme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ComponentsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_components)
+    }
+}
